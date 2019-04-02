f16b161d7aa95d83ef98779fe965ac7bb1c9ea6c
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-04-02 13:51:25 +0800

    rockchip: vendor: Fix maybe-uninitialized
    arch/arm/mach-rockchip/vendor.c:663:6  error: 'item_num' may be used uninitialized in this function [-Werror=maybe-uninitialized]
      ret = vendor_storage_write(id, buffer, size);
      ~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Change-Id: Ie80c345f6b8008fd4b46ba54788a6ac10d94e4f1
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- arch/arm/mach-rockchip/vendor.c
+++ arch/arm/mach-rockchip/vendor.c
@@ -784,7 +784,9 @@ int vendor_storage_test(void)
 		size = total_size / item_num;
 		break;
 	default:
-		total_size = 0;
+		item_num = 0;
+ 		total_size = 0;
+		size = 0;
 		break;
 	}
 	/* Invalid bootdev? */
